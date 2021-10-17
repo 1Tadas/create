733980457ef57f384f8fb6875bb13bc9fa2fa132
Sergei <work@sergeis-mbp.home>
2021-10-17 13:40:18 +0100

    logo removed

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,7 +7,6 @@ export default function Main() {
   return (
     <>
 <header class="bg-white w-full h-12 flex justify-center items-center shadow-sm fixed z-50">
-<img class="w-16" src="https://raw.githubusercontent.com/1Tadas/img/5b3c8363ebc7293696a0c44826d51378b2738d1b/tklogo.svg" alt=""></img>
 </header>
 <div class="bg-green-400 w-screen h-screen">
 
