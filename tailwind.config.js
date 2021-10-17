2a612aff538542dd89e9748d37159f68fc379211
Sergei <work@sergeis-mbp.home>
2021-10-17 13:53:03 +0100

    logo removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+   purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
